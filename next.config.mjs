b74497e2328bffb2d60812adcfbb19733ee073e5
appelmoes1234 <moestafaamid@hotmail.com>
2026-06-18 14:09:35 +0200

    Remove unsupported turbopack config for Next 14

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
